be0390be5d36e20dad622f95e2da1597609ad4c7
Nathanael Kim <nateykim@gmail.com>
2023-02-27 23:27:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.150.0/build/three.module.js';
 
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
@@ -9,4 +8,21 @@ console.log(camera);
 console.log(renderer);
 
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio) //set dpi to device specs
 document.body.appendChild(renderer.domElement)
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
+const mesh = new THREE.Mesh(boxGeometry, material)
+
+scene.add(mesh)
+
+camera.position.z = 5
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    mesh.rotation.x += 0.1
+}
+
+animate()
