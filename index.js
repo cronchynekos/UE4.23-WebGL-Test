2e45b557b152f3174f6e0adbd785576b2393927d
Nathanael Kim <nateykim@gmail.com>
2023-02-28 00:30:13 -0800

    Created a 3d plane

--- index.js
+++ index.js
@@ -3,26 +3,40 @@ import * as THREE from 'https://unpkg.com/three@0.150.0/build/three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
-console.log(scene);
-console.log(camera);
-console.log(renderer);
 
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio) //set dpi to device specs
 document.body.appendChild(renderer.domElement)
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
-const mesh = new THREE.Mesh(boxGeometry, material)
-
-scene.add(mesh)
-
 camera.position.z = 5
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+const planeMaterial = new THREE.
+    MeshPhongMaterial({
+    color: 0xff0000, 
+    side: THREE.DoubleSide,
+})
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+console.log(planeMesh)
+planeMesh.material.flatShading = true
+scene.add(planeMesh)
+
+const {array} = planeMesh.geometry.attributes.position
+for (let i = 0; i < array.length; i += 3) {
+    const x = array[i]
+    const y = array[i + 1]
+    const z = array[i + 2]
+    array[i + 2] = z + Math.random()
+}
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(0, 0, 1)
+scene.add(light)
 
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    mesh.rotation.x += 0.1
+    // planeMesh.rotation.x += 0.01
 }
 
 animate()
