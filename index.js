d3a86b9ba19b6237cff77948669c3911a97f547d
Nathanael Kim <nateykim@gmail.com>
2023-03-05 23:29:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import {OrbitControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import dat from "https://cdn.skypack.dev/dat.gui";
 
+
+//datgui setup
 const gui = new dat.GUI()
 const world = {
     plane: {
@@ -34,28 +36,30 @@ function generatePlane() {
     }
 }
 
+//scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
-
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio) //set dpi to device specs
-document.body.appendChild(renderer.domElement)
+document.body.appendChild(renderer.domElement) //set the canvas to the html body
+const raycaster = new THREE.Raycaster() //tells 3js where the mouse is
 
 new OrbitControls(camera, renderer.domElement)
 camera.position.z = 5
 
+//material setup
 const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10)
 const planeMaterial = new THREE.
     MeshPhongMaterial({
-    color: 0xff0000, 
     side: THREE.DoubleSide,
+    vertexColors: true
 })
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
-console.log(planeMesh)
 planeMesh.material.flatShading = true
 scene.add(planeMesh)
 
+//geometry gradient
 const {array} = planeMesh.geometry.attributes.position
 for (let i = 0; i < array.length; i += 3) {
     const x = array[i]
@@ -64,6 +68,14 @@ for (let i = 0; i < array.length; i += 3) {
     array[i + 2] = z + Math.random()
 }
 
+const colors = []
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(1,0,0)
+}
+
+planeMesh.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3))
+
+//light setup
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0, 0, 1)
 scene.add(light)
@@ -72,10 +84,45 @@ const backlight = new THREE.DirectionalLight(0xffffff, 1)
 backlight.position.set(0, 0, -1)
 scene.add(backlight)
 
+//define mouse for coordinates
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+//animate
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    // planeMesh.rotation.x += 0.01
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObject(planeMesh)
+    if (intersects.length > 0) {
+
+        const {color} = intersects[0].object.geometry.attributes
+
+        //vertice 1
+        color.setX(intersects[0].face.a, 0)
+        color.setY(intersects[0].face.a, 0)
+        color.setZ(intersects[0].face.a, 1)
+
+        //vertice 2
+        color.setX(intersects[0].face.b, 0)
+        color.setY(intersects[0].face.b, 0)
+        color.setZ(intersects[0].face.b, 1)
+
+        //vertice 3
+        color.setX(intersects[0].face.c, 0)
+        color.setY(intersects[0].face.c, 0)
+        color.setZ(intersects[0].face.c, 1)
+        intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+    }
 }
 
 animate()
+
+
+addEventListener('mousemove', () => {
+     mouse.x = (event.clientX / innerWidth) * 2 - 1
+     mouse.y = -(event.clientY / innerHeight) * 2 + 1
+})
