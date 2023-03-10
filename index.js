d1bdaf6f5f058954dd5d2a215aa2acb929e13287
Nathanael Kim <nateykim@gmail.com>
2023-03-10 14:09:42 -0800

    commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
+// import "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js";
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import {OrbitControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import dat from "https://cdn.skypack.dev/dat.gui";
 
+console.log(gsap)
 
 //datgui setup
 const gui = new dat.GUI()
@@ -68,9 +71,10 @@ for (let i = 0; i < array.length; i += 3) {
     array[i + 2] = z + Math.random()
 }
 
+//initial color
 const colors = []
 for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
-    colors.push(1,0,0)
+    colors.push(0, 0.19, 0.4)
 }
 
 planeMesh.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3))
@@ -101,21 +105,52 @@ function animate() {
         const {color} = intersects[0].object.geometry.attributes
 
         //vertice 1
-        color.setX(intersects[0].face.a, 0)
-        color.setY(intersects[0].face.a, 0)
+        color.setX(intersects[0].face.a, 0.1)
+        color.setY(intersects[0].face.a, 0.5)
         color.setZ(intersects[0].face.a, 1)
 
         //vertice 2
-        color.setX(intersects[0].face.b, 0)
-        color.setY(intersects[0].face.b, 0)
+        color.setX(intersects[0].face.b, 0.1)
+        color.setY(intersects[0].face.b, 0.5)
         color.setZ(intersects[0].face.b, 1)
 
         //vertice 3
-        color.setX(intersects[0].face.c, 0)
-        color.setY(intersects[0].face.c, 0)
+        color.setX(intersects[0].face.c, 0.1)
+        color.setY(intersects[0].face.c, 0.5)
         color.setZ(intersects[0].face.c, 1)
         intersects[0].object.geometry.attributes.color.needsUpdate = true
 
+        const initialColor = {
+            r: 0,
+            g: .19,
+            b: .4
+        }
+        const hoverColor = {
+            r: 0.1,
+            g: .5,
+            b: 1
+        }
+
+        gsap.to(hoverColor, {
+            r: initialColor.r,
+            g: initialColor.g,
+            b: initialColor.b,
+            onUpdate: () => {
+                color.setX(intersects[0].face.a, hoverColor.r)
+                color.setY(intersects[0].face.a, hoverColor.g)
+                color.setZ(intersects[0].face.a, hoverColor.b)
+
+                //vertice 2
+                color.setX(intersects[0].face.b, hoverColor.r)
+                color.setY(intersects[0].face.b, hoverColor.g)
+                color.setZ(intersects[0].face.b, hoverColor.b)
+
+                //vertice 3
+                color.setX(intersects[0].face.c, hoverColor.r)
+                color.setY(intersects[0].face.c, hoverColor.g)
+                color.setZ(intersects[0].face.c, hoverColor.b)
+            }
+        })
     }
 }
 
